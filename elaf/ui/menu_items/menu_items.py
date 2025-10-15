3ecec3ec4ad73b6984509d6222c934cedf6b3121
jeme shaheen <47688567+ahmedshaheen201400721@users.noreply.github.com>
2025-10-15 14:48:30 +0300

    fix migation

--- elaf/ui/menu_items/menu_items.py
+++ elaf/ui/menu_items/menu_items.py
@@ -1,34 +1,17 @@
-# -*- coding: utf-8 -*-
-# Menu items for elaf module
 
-# {
-#     "elaf_main_menu": {
-#         "name": "elaf",
-#         "icon": "elaf",
-#         "module": "elaf",
-#         "sequence": 101,
-#         'children': {
-#             "elaf_main_menu_model1": {
-#                 "name": "model1",
-#                 "icon": "model1",
-#                 "module": "elaf",
-#                 "model": "elaf.model1",
-#                 "sequence": 1,
-#             },
-#             "elaf_main_menu_model2": {
-#                 "name": "model2",
-#                 "icon": "model2",
-#                 "module": "elaf",
-#                 "model": "elaf.model2",
-#                 "sequence": 2,
-#             },
-#             "elaf_main_menu_model3": {
-#                 "name": "model3",
-#                 "icon": "model3",
-#                 "module": "elaf",
-#                 "model": "elaf.model3",
-#                 "sequence": 3,
-#             }
-#         }
-#     }
-# }
+
+
+menu_dict = {
+    "set_default_filter_for_contacts": {
+        "_inherit": "contacts_main_menu_partner",
+        "inheritance_operations": [
+           {
+                "operation": "merge",
+                "target": "context",
+                "content": {
+                    "default_filter": 'my_not_contacted_filter'
+                }
+            },
+        ]
+    }
+}
